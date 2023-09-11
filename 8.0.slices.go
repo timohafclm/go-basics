eec013296708baad45c71baed2adc6aa9b328a40
igor <timohafclm@mail.ru>
2023-09-11 22:06:09 +0300

    slices

--- 8.0.slices.go
+++ 8.0.slices.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
+// slices are wrapper references on top of arrays
+
 var newSlice = []int{4, 9, 2, 5}
+var newSliceString = []string{"one", "two"}
 
 var sliceViaMake = make([]int, 3, 10)
 
@@ -10,7 +13,7 @@ func sliceFromArray() {
 	array := [5]int{1, 2, 5, 10, 3}
 	slice := array[0:3]
 
-	fmt.Print(slice)
+	fmt.Print(slice) // [1 2 5]
 }
 
 func underlyingArrayIsChanged() {
@@ -25,7 +28,7 @@ func appendExample() {
 	fmt.Println(len(newSlice)) // 4
 	fmt.Println(cap(newSlice)) // 4
 
-	appended := append(newSlice, 2)
+	appended := append(newSlice, 2) // append() returns new slice
 
 	fmt.Println(appended)      // [4 9 2 5 2]
 	fmt.Println(len(appended)) // 5
@@ -34,4 +37,8 @@ func appendExample() {
 	fmt.Println(newSlice)      // [4 9 2 5]
 	fmt.Println(len(newSlice)) // 4
 	fmt.Println(cap(newSlice)) // 4
+}
+
+func main() {
+	sliceFromArray()
 }
